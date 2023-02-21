0d7417816448adaef5e28ff4ef8fdb263cc05e90
JFcoDiaz <jfcodiaz@gmail.com>
2023-02-21 12:31:54 -0600

    28 Using Cascading Inserts and Updates

--- app/ilovecoffe/src/coffees/entities/coffee.entity.ts
+++ app/ilovecoffe/src/coffees/entities/coffee.entity.ts
@@ -16,7 +16,10 @@ export class Coffee {
   @JoinTable()
   @ManyToMany(
     type => Flavor,
-    flavor => flavor.coffees
+    flavor => flavor.coffees,
+    {
+      cascade: true
+    }
   )
-  flavors: string[];
+  flavors: Flavor[];
 }
