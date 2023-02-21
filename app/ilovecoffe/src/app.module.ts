3138911b68fc00d2dec8083b47ccd82fdc13a59c
JFcoDiaz <jfcodiaz@gmail.com>
2023-02-21 02:44:19 -0600

    22 Introduciong the TypeORM Module

--- app/ilovecoffe/src/app.module.ts
+++ app/ilovecoffe/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'db',
+    port: 5432,
+    username: 'postgres',
+    password: 'pass123',
+    autoLoadEntities: true,
+    synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
