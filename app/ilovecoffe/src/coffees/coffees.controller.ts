c40d6a533fec441622c27a84f34840e593b430e7
JFcoDíaz <jfcodiaz@gmail.com>
2023-02-21 04:06:42 +0000

    11 Handling Update and Delete Requests

--- app/ilovecoffe/src/coffees/coffees.controller.ts
+++ app/ilovecoffe/src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
 import { response } from 'express';
 
 @Controller('coffees')
@@ -15,6 +15,16 @@ export class CoffeesController {
 
   @Post()
   create(@Body() body) {
-    return body.name;
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action update #${id} coffee`
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action remove #${id} coffee`;
   }
 }
