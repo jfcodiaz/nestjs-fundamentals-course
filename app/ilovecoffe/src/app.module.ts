e62e286e6e88cedfef08297ccafb7f93e712e768
JFcoDiaz <jfcodiaz@gmail.com>
2023-02-23 00:50:49 -0600

    32 implement ConfigModule

--- app/ilovecoffe/src/app.module.ts
+++ app/ilovecoffe/src/app.module.ts
@@ -1,19 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'db',
-    port: 5432,
-    username: 'postgres',
-    password: 'pass123',
-    autoLoadEntities: true,
-    synchronize: true
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `./env/.${process.env.NODE_ENV}.env`,
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    CoffeesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- app/ilovecoffe/src/main.ts
+++ app/ilovecoffe/src/main.ts
@@ -1,9 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
@@ -12,6 +15,6 @@ async function bootstrap() {
       enableImplicitConversion: true
     }
   }));
-  await app.listen(3000);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
